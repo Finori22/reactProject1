019bdb1922fcb28314ebe4ef62bc723a1612dc1a
Lucjan Dybczak <lucjan.dybczak@gmail.com>
2022-07-13 14:10:58 +0200

    done

--- landing-page-app/src/components/Main.js
+++ landing-page-app/src/components/Main.js
@@ -1,13 +1,36 @@
 import Specialist from "./subComponents/specialist";
 import "./CSS/main.css";
 
+const employees = [
+  {
+    name: "Jonny Nowak",
+    position: "Senior React Developer",
+    about:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vel massa et lacus egestas cursus a non magna. Fusce scelerisque blandit nunc, id malesuada ex lobortis a. Integer felis nisi, tempor elementum lorem in, varius pellentesque ligula. Duis efficitur lacinia enim, non tincidunt libero ultrices in.",
+    photoUrl: "/specialist-1.jpg",
+  },
+  {
+    name: "Antony Donat",
+    position: "Regular .Net Engineer",
+    about:
+      "Lorem ipsum dolor sisfsdfds, consectetur adipiscing elit. Morbi vel massa et lacus egestas cursus a non magna. Fusce scelerisque blandit nunc, id malesuada ex lobortis a. Integer felis nisi, tempor elementum lorem in, varius pellentesque ligula. Duis efficitur lacinia enim, non tincidunt libero ultrices in.",
+    photoUrl: "/specialist-2.jpg",
+  },
+];
+
 const Main = () => {
   return (
     <main class="main-section" id="aboutUsSection">
       <div class="main-container">
         <h1 class="title-main">Our specialists</h1>
-        <Specialist />
-        <Specialist />
+        {employees.map((employee) => (
+          <Specialist
+            name={employee.name}
+            position={employee.position}
+            about={employee.about}
+            photoUrl={employee.photoUrl}
+          />
+        ))}
       </div>
     </main>
   );
--- landing-page-app/src/components/subComponents/specialist.js
+++ landing-page-app/src/components/subComponents/specialist.js
@@ -1,18 +1,14 @@
 import "../CSS/specialist.css";
 
-const Specialist = () => {
+const Specialist = (props) => {
   return (
     <div class="specialist-container">
-      <img class="profile" alt="Jonny Nowak"></img>
+      <img src={props.photoUrl} class="profile" alt="Jonny Nowak"></img>
       <div class="specialist-description">
-        <h2>Jonny Nowak</h2>
-        <p>
-          Imię Nazwisko [ dział ] Lorem ipsum dolor sit amet, consectetur
-          adipiscing elit. Morbi vel massa et lacus egestas cursus a non magna.
-          Fusce scelerisque blandit nunc, id malesuada ex lobortis a. Integer
-          felis nisi, tempor elementum lorem in, varius pellentesque ligula.
-          Duis efficitur lacinia enim, non tincidunt libero ultrices in.
-        </p>
+        <h2>
+          {props.name} ({props.position})
+        </h2>
+        <p>{props.about}</p>
       </div>
     </div>
   );
